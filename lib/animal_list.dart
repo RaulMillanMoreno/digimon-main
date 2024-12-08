648ba11ab19336e2e10896db604eb0f4fb37b208
RaulMillanMoreno <raul.millan@sarria.salesians.cat>
2024-12-08 18:40:57 +0100

    documentació del animal_list

--- lib/animal_list.dart
+++ lib/animal_list.dart
@@ -1,22 +1,21 @@
 import 'package:digimon/animal_card.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; 
 import 'animal_model.dart';
 
 class AnimalList extends StatelessWidget {
-  final List<Animal> animals;
+  final List<Animal> animals; // Lista de animales a mostrar.
   const AnimalList(this.animals, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return _buildList(context);
+    return _buildList(context); 
   }
 
-  ListView _buildList(context) {
+  ListView _buildList(context) { // Construye una lista con los animales que hemos teniamos en el main.   
     return ListView.builder(
-      itemCount: animals.length,
-      // ignore: avoid_types_as_parameter_names
+      itemCount: animals.length, // Número de elementos en la lista.
       itemBuilder: (context, int) {
-        return AnimalCard(animals[int]);
+        return AnimalCard(animals[int]); // Para cada índice, crea una tarjeta con información del animal.        
       },
     );
   }
