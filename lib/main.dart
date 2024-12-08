18ec14ae35cb7943858fce982e3345dbf2a79fb1
RaulMillanMoreno <raul.millan@sarria.salesians.cat>
2024-12-08 18:07:45 +0100

    comentarios-main

--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,72 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'animal_model.dart';
-import 'animal_list.dart';
+import 'animal_list.dart'; 
 import 'new_animal_form.dart';
 
+void main() => runApp(const MyApp()); 
 
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget { // Pantalla principal.
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'The animals',//canbias el nombre
+      title: 'The animals',
       theme: ThemeData(brightness: Brightness.dark),
-      home: const MyHomePage(//canbias el nombre
+      home: const MyHomePage( 
         title: 'The animals',
       ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatefulWidget { 
   final String title;
   const MyHomePage({super.key, required this.title});
 
   @override
-  // ignore: library_private_types_in_public_api
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<Animal> initialAnimals = [Animal('cat'), Animal('dog'), Animal('bird')];
-  Future _showNewAnimalForm() async {//para que no pete al crear uno nuevo, ponemos el ? en el Animal y ponemos la inicializacion en un if para asegurar que siempre iniciara un animal real. si no peta
-    Animal? newAnimal = await Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
-      return const AddAnimalFormPage();
-    }));
-    if (newAnimal != null){
+  List<Animal> initialAnimals = [Animal('cat'), Animal('dog'), Animal('bird')]; // Lista inicial de animales a mostrar.
+  Future _showNewAnimalForm() async {// Abre el formulario para añadir un nuevo animal y actualizar la lista.    
+    Animal? newAnimal = await Navigator.of(context).push(MaterialPageRoute(
+      builder: (BuildContext context) {
+        return const AddAnimalFormPage(); // Navega a la pantalla donde se agregar nuevos animales.
+      },
+    ));
+    if (newAnimal != null) { // Verifica que el nuevo animal no sea nulo y lo agrega la lista a la lista.
       initialAnimals.add(newAnimal);
-      setState(() {});
-    }    
+      setState(() {}); // Actualiza la interfaz.
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var key = GlobalKey<ScaffoldState>();
     return Scaffold(
-      key: key,
       appBar: AppBar(
-        title: Text(widget.title, style: const TextStyle(color: Colors.green)),
+        title: Text(widget.title, style: const TextStyle(color: Colors.green)), 
         centerTitle: true,
         backgroundColor: const Color.fromARGB(255, 0, 0, 0),
-        actions: <Widget>[
+        actions: <Widget>[ 
           Padding(
             padding: const EdgeInsets.only(right: 20.0),
-            child: IconButton(
+            child: IconButton( // Botón para agregar un nuevo animal.
               icon: const Icon(Icons.add),
               color: Colors.green,
-              onPressed: _showNewAnimalForm,
+              onPressed: _showNewAnimalForm,// Muestra el formulario al presionarlo.
             ),
           ),
         ],
       ),
       body: Container(
-          color: const Color.fromARGB(255, 143, 157, 174),
-          child: Center(
-            child: AnimalList(initialAnimals),
-          )),
+        color: const Color.fromARGB(255, 143, 157, 174),
+        child: Center(
+          child: AnimalList(initialAnimals), // Muestra la lista de los animales iniciales.
+        ),
+      ),
     );
   }
 }
